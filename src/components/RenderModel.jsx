55bf97f627a113812ad14d4a3e561d72f8561317
Hannah G Kim <hannahgomezkim@gmail.com>
2024-10-24 23:39:25 +0900

    three js fix 1

--- src/components/RenderModel.jsx
+++ src/components/RenderModel.jsx
@@ -1,9 +1,36 @@
 "use client";
 import { Canvas } from "@react-three/fiber";
 import clsx from "clsx";
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect } from "react";
 
 const RenderModel = ({ children, className }) => {
+  useEffect(() => {
+    const canvas = document.querySelector("canvas");
+
+    const handleContextLost = (event) => {
+      event.preventDefault();
+      console.warn("WebGL context lost. Rendering paused.");
+      // You can trigger a message, retry, or attempt a re-render here if needed
+    };
+
+    const handleContextRestored = () => {
+      console.log("WebGL context restored. Rendering resumed.");
+      // You can reinitialize or refresh the scene here if necessary
+    };
+
+    canvas.addEventListener("webglcontextlost", handleContextLost, false);
+    canvas.addEventListener(
+      "webglcontextrestored",
+      handleContextRestored,
+      false
+    );
+
+    return () => {
+      canvas.removeEventListener("webglcontextlost", handleContextLost);
+      canvas.removeEventListener("webglcontextrestored", handleContextRestored);
+    };
+  }, []);
+
   return (
     <Canvas
       className={clsx("w-screen h-screen -z-10 relative", className)}
